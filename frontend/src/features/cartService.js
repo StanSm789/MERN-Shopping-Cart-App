132ea82e215e6ab6c4b5642c36d7c5b73f892751
Stanislav Smirnov <stanislav.smirnov@griffithuni.edu.au>
2022-10-02 12:08:25 +1000

    completed migrating cartSlice under features

--- frontend/src/features/cartService.js
+++ frontend/src/features/cartService.js
@@ -1,35 +1,35 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const API_URL = '/api/carts'
+const API_URL = '/api/carts'
 
-// // Create new cart
-// const createCart = async (cartData) => {
+// Create new cart
+const createCart = async (cartData) => {
 
-//   const response = await axios.post(API_URL, cartData)
+  const response = await axios.post(API_URL, cartData)
 
-//   return response.data
-// }
+  return response.data
+}
 
-// // Get carts
-// const getCarts = async () => {
+// Get carts
+const getCarts = async () => {
 
-//   const response = await axios.get(API_URL)
+  const response = await axios.get(API_URL)
 
-//   return response.data
-// }
+  return response.data
+}
 
-// // Delete cart
-// const deleteCart = async (cartId) => {
+// Delete cart
+const deleteCart = async (cartId) => {
 
-//   const response = await axios.delete(API_URL + cartId)
+  const response = await axios.delete(API_URL + cartId)
 
-//   return response.data
-// }
+  return response.data
+}
 
-// const cartService = {
-//     createCart,
-//     getCarts,
-//     deleteCart,
-// }
+const cartService = {
+    createCart,
+    getCarts,
+    deleteCart,
+}
 
-// export default cartService
+export default cartService
--- frontend/src/features/cartSlice.js
+++ frontend/src/features/cartSlice.js
@@ -1,10 +1,71 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import cartService from './cartService'
+
+const initialState = {
+  cart: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
+
+// Create new cart
+export const createCart = createAsyncThunk(
+  'carts/create',
+  async (cartData, thunkAPI) => {
+    try {
+      return await cartService.createItem(cartData)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+// Get carts
+export const getCarts = createAsyncThunk(
+  'carts/getAll',
+  async (_, thunkAPI) => {
+    try {
+      return await cartService.getCarts()
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+// Delete cart
+export const deleteCart = createAsyncThunk(
+  'carts/delete',
+  async (id, thunkAPI) => {
+    try {
+      return await cartService.deleteCart(id)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
 
 export const cartSlice = createSlice({
   name: 'cart',
-  initialState: {
-    cart: [],
-  },
+  initialState,
   reducers: {
     addToCart: (state, action) => {
       if (state.cart.find(x => x._id === action.payload._id)) {
@@ -42,9 +103,53 @@ export const cartSlice = createSlice({
         return obj
       })
     },
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createCart.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createCart.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.cart.push(action.payload)
+      })
+      .addCase(createCart.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(getCarts.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getCarts.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.cart = action.payload
+      })
+      .addCase(getCarts.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(deleteCart.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(deleteCart.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.cart = state.cart.filter(
+          (item) => item._id !== action.payload._id
+        )
+      })
+      .addCase(deleteCart.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
   },
 })
 
-export const { addToCart, removeFromCart, increment, decrement } = cartSlice.actions
-
+export const { reset, addToCart, removeFromCart, increment, decrement } = cartSlice.actions
 export default cartSlice.reducer
