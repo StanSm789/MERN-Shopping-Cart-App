c5371bd24a9b9e39a27be2cc762f80cf24a4952c
Stanislav Smirnov <stanislav.smirnov@griffithuni.edu.au>
2022-10-04 01:29:55 +1000

    backend cleanup

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/items', require('./routes/itemRoutes'))
 app.use('/api/carts', require('./routes/cartRoutes'))
 
